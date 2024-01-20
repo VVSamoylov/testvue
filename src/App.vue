ad191cb19d129a6d0fc9f0b9c3f6b5b79dd30aba
Veniamin <bm0651@yandex.ru>
2024-01-20 18:24:32 +0300

    lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Card
+   descr=" Терминатор самый терминорный в мире!!"
+  foto="https://w.forfun.com/fetch/94/942dfb4a14880c75031162b342be210d.jpeg?w=1470&r=0.5625"
+   msg="Это картинка терминатора!!"/>
+  <Card
+  foto="https://cojo.ru/wp-content/uploads/2023/01/figurka-ksenomorfa-chuzhoi-zavet-alien-17sm-1.webp" 
+  msg="Чужой!!"
+  descr="Бойся паразотов они опасны!!" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
