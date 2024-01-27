4a8e46b3bba04e354bfb58880c1c184b12abed19
Veniamin <bm0651@yandex.ru>
2024-01-27 18:29:52 +0300

    lesson2

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+class Product{
+    PName
+    Price
+    PVolume
+    constructor(iName, iPrice, iVolume){
+        this.PName = iName
+        this.Price = iPrice
+        this.PVolume = iVolume
+    }
+
+}
+
+export default Product
---
+++ src/components/card.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="rows">
+    <form class="row g-3">
+      <div class="col-auto">
+        <label for="inputName" class="visually-hidden">Название</label>
+        <input v-model="part.PName" type="text" class="form-control" id="inputName" placeholder="Название">
+      </div>
+      <div class="col-auto">
+        <label for="inputPrace" class="visually-hidden">Цена за шт</label>
+        <input v-model="part.Price" type="text" class="form-control" id="inputPrace" placeholder="Цена за шт">
+      </div>
+      <div class="col-auto">
+        <label for="inputVolume" class="visually-hidden">Кол - во</label>
+        <input v-model="part.PVolume" type="text" class="form-control" id="inputVolume" placeholder="Кол - во">
+      </div>
+      
+      <div class="col-auto">
+        <button v-on:click="added"  class="btn btn-primary mb-3">Добавить</button>
+      </div>
+    </form>
+
+    <button v-on:click="show"  class="btn btn-primary mb-3">Посмотреть результат</button>
+
+    <div v-if="check" class="row">
+      <ul id="array-rendering">
+        <li v-for="item in products" :key="item"  >
+          {{ item.PName }}   {{ item.Price }} {{ item.PVolume }} <br/>
+          
+        </li>
+      </ul>
+
+
+    </div>
+
+    <div v-if="check" class="row">
+      <ul id="array-rendering">
+        <li v-for="item in test" :key="item"  >
+          {{ item }}  
+          
+        </li>
+      </ul>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+/* eslint-disable */ 
+import Product from '../Product.js'
+export default {
+  name: 'card',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      products: [],
+
+      part: Product,
+      check: false,
+      test:[1,2,3,4]
+    }
+  },
+  methods:{
+    added(){
+      
+      this.products.push(this.part)
+      //alert(this.product[0].PName)
+      
+    },
+    show(){
+      this.check = true
+      //alert(this.product[0].PName)
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
